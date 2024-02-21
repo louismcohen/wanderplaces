34f48f420e775a6863db84c7729315baf92b6aa5
d3lm3l <d3lm3l@gmail.com>
2024-02-20 23:42:47 -0800

    tab bar implementation

--- App.js
+++ App.js
@@ -3,12 +3,51 @@ import { StyleSheet, View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import PlacesMap from './PlacesMap';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Entypo, FontAwesome, FontAwesome5 } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+const TableView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is the table view</Text>
+    </View>
+  )
+}
+
 export default App = () => {
   return (
-    <View style={ styles.container }>
-      <StatusBar style='auto' />
-      <PlacesMap />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator 
+        screenOptions={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontSize: 13,
+          }}}
+      >
+        <Tab.Screen 
+          name='Places' 
+          component={TableView} 
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <FontAwesome name='th-list' color={color} size={size} />
+            )
+          }}  
+        />
+        <Tab.Screen 
+          name='Map' 
+          component={PlacesMap} 
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <FontAwesome5 name='map' color={color} size={size} />
+            )
+          }}          
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 };
 
